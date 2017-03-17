1fbcb78910e8ef91a158a34a65d5fef778cf0251
Shinigami <Shinigami@192.168.1.39>
2017-03-17 10:53:25 +0100

    Primer commit

---
+++ ProyectoGit/src/clases/Calculadora.java
@@ -0,0 +1,38 @@
+package clases;
+/**
+ * 
+ * Esta clase suma, resta, multiplica y divide.
+ * @author Shinigami
+ * @version 1.0 10/02/2017
+ * 
+ *
+ */
+
+public class Calculadora {
+/**
+ * 
+ * @param x primer sumando
+ * @param y segundo sumando
+ * @return x+y
+ */
+
+	public float suma(float x, float y){
+		return (x+y);
+	}
+	public static float resta(float x, float y){
+		return (x - y);
+	}
+	public static float multiplicacion(float x, float y){
+		return (x * y);
+	}
+	/**
+	 * Divide x entre y
+	 * @param x dividendo
+	 * @param y divisor
+	 * @return cociente
+	 */
+	public static float division(float x, float y){
+		return (x / y);
+	}
+
+}
---
+++ ProyectoGit/src/principal/Main.java
@@ -0,0 +1,18 @@
+package principal;
+
+import clases.Calculadora;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calculadora c1 = new Calculadora();
+		System.out.println(c1.suma(2, 2));
+	}
+
+}
